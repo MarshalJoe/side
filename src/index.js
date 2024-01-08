0dfca05477fe55251f5f198c84d96c82b3a14ea0
MarshalJoe <joseph.charles.marshall@gmail.com>
2024-01-08 12:34:41 -0600

    implement react router and add basic error page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap';
+import './styles';
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App';
+import ErrorPage from "./error";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,3 @@
 // Style baseline to work with. Feel free to remove if you prefer something
 // else.
-import 'bootstrap/dist/css/bootstrap-reboot.css';
 import './styles/global.css';
